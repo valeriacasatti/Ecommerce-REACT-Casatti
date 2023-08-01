be467244be27a62fde3058dbf194cc9843969a17
Valeria Casatti <valeria.casatti@gmail.com>
2023-08-01 12:19:37 -0300

    se agregan variables de entorno

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD8OQWkfq7H4mVMEG5CbaHpdPYUKislH-I",
-  authDomain: "proyectofinal-casatti.firebaseapp.com",
-  projectId: "proyectofinal-casatti",
-  storageBucket: "proyectofinal-casatti.appspot.com",
-  messagingSenderId: "249423929917",
-  appId: "1:249423929917:web:cb9b243f29d9faf6a9c054",
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
